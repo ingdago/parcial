bf4c39c3e1d6b5dc069083ce5a834df4289cd63d
dagoberto <dagoberto0805@gmail.com>
2019-09-20 22:07:05 -0500

    commit de prueba parcial del corte 1

---
+++ parcial/src/app/grupoa/grupoa.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GrupoaRoutingModule } from './grupoa-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GrupoaRoutingModule
+  ]
+})
+export class GrupoaModule { }
