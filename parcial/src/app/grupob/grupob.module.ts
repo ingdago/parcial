84d15a41ab8de6b18b8a0b6fe4bb69a824c4eb12
dagoberto <dagoberto0805@gmail.com>
2019-09-20 22:13:47 -0500

    prueba1

---
+++ parcial/src/app/grupob/grupob.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GrupobRoutingModule } from './grupob-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    GrupobRoutingModule
+  ]
+})
+export class GrupobModule { }
